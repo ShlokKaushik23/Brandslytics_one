694ff8f25dd533b6b4d538ba444065165bf23e4c
Shlok Kaushik <54725967+ShlokKaushik23@users.noreply.github.com>
2024-09-17 16:30:52 +0530

    Project 01

---
+++ public/script/script.js
@@ -0,0 +1,366 @@
+const galleriesData = [
+    {
+        id: 1,
+        images: [
+           
+            "./public/images/projects/metal/metaljewellery_1.webp",
+            "./public/images/projects/metal/metaljewellery_2.webp",
+            "./public/images/projects/metal/metaljewellery_3.webp",
+            "./public/images/projects/metal/metaljewellery_4.webp",
+            "./public/images/projects/metal/metaljewellery_5.webp",
+            "./public/images/projects/metal/metaljewellery_6.webp",
+            "./public/images/projects/metal/metaljewellery_7.webp",
+            "./public/images/projects/metal/metaljewellery_8.webp",
+            "./public/images/projects/metal/metaljewellery_9.webp",
+            "./public/images/projects/metal/metaljewellery_10.webp",
+            "./public/images/projects/metal/metaljewellery_11.webp",
+            "./public/images/projects/metal/metaljewellery_12.webp",
+
+
+
+        ]
+    },
+    {
+        id: 2,
+        images: [
+            "./public/images/projects/02_Proj/project1.JPG",
+            "./public/images/projects/02_Proj/project2.JPG",
+            "./public/images/projects/02_Proj/project3.JPG",
+            "./public/images/projects/02_Proj/project4.JPG",
+            "./public/images/projects/02_Proj/project5.JPG",
+            "./public/images/projects/02_Proj/project6.JPG",
+
+        ]
+    },
+    {
+        id: 3,
+        images: [
+            "./public/images/projects/11_TheForestindoors/Nitish-Jain_The-Forest-Indoors_1.webp",
+            "./public/images/projects/11_TheForestindoors/Nitish-Jain_The-Forest-Indoors_2.webp",
+            "./public/images/projects/11_TheForestindoors/Nitish-Jain_The-Forest-Indoors_3.webp",
+            "./public/images/projects/11_TheForestindoors/Nitish-Jain_The-Forest-Indoors_4.webp",
+            "./public/images/projects/11_TheForestindoors/Nitish-Jain_The-Forest-Indoors_5.webp",
+
+
+        ]
+    },
+    {
+        id: 4,
+        images: [
+            "./public/images/projects/10_BodySpaceSensing/Nitish-Jain_Body-Space-Sensing_1.webp",
+            "./public/images/projects/10_BodySpaceSensing/Nitish-Jain_Body-Space-Sensing_2.webp",
+            "./public/images/projects/10_BodySpaceSensing/Nitish-Jain_Body-Space-Sensing_3.webp",
+            "./public/images/projects/10_BodySpaceSensing/Nitish-Jain_Body-Space-Sensing_4.webp",
+            "./public/images/projects/10_BodySpaceSensing/Nitish-Jain_Body-Space-Sensing_5.webp",
+
+
+        ]
+    },
+    {
+        id: 5,
+        images: [
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_1.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_2.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_3.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_4.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_5.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_6.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_7.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_8.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_9.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_10.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_11.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_12.webp",
+            "./public/images/projects/05_TicklishFruits/Nitish-Jain_Ticklish-Fruits_13.webp",
+
+
+        ]
+    },
+    {
+        id: 6,
+        images: [
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_1.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_2.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_3.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_4.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_5.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_6.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_7.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_8.webp",
+            "./public/images/projects/06_a-where-ness/Nitish-Jain_a-where-ness_9.webp",
+
+        ]
+    },
+    {
+        id: 7,
+        images: [
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_1.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_2.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_3.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_4.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_5.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_6.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_7.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_8.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_9.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_10.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_11.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_12.webp",
+            "./public/images/projects/01_Spoonfed/Nitish-Jain_Spoonfed_13.webp",
+
+
+
+        ]
+    },
+    {
+        id: 8,
+        images: [
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_1.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_2.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_3.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_5.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_6.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_7.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_8.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_9.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_10.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_11.webp",
+            "./public/images/projects/08_Fcilespressobar/Nitish-Jain_Fčil-Espresso-Bar_12.webp",
+
+        ]
+    },
+    {
+        id: 9,
+        images: [
+            "./public/images/projects/12_Chromoculars/Nitish-Jain_Chromoculars_1.webp",
+            "./public/images/projects/12_Chromoculars/Nitish-Jain_Chromoculars_2.webp",
+            "./public/images/projects/12_Chromoculars/Nitish-Jain_Chromoculars_3.webp",
+            "./public/images/projects/12_Chromoculars/Nitish-Jain_Chromoculars_4.webp",
+            "./public/images/projects/12_Chromoculars/Nitish-Jain_Chromoculars_5.webp",
+            "./public/images/projects/12_Chromoculars/Nitish-Jain_Chromoculars_6.webp",
+        ]
+    },
+    {
+        id: 10,
+        images: [
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_1.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_2.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_3.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_4.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_5.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_6.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_7.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_8.webp",
+            "./public/images/projects/03_Water_Sprite/Nitish-Jain_Water-Sprite_9.webp",
+
+
+        ]
+    },
+    {
+        id: 11,
+        images: [
+            "./public/images/projects/04_Treasure/Nitish-Jain_Treasure_1.webp",
+            "./public/images/projects/04_Treasure/Nitish-Jain_Treasure_2.webp",
+            "./public/images/projects/04_Treasure/Nitish-Jain_Treasure_3.webp",
+            "./public/images/projects/04_Treasure/Nitish-Jain_Treasure_4.webp",
+            "./public/images/projects/04_Treasure/Nitish-Jain_Treasure_5.webp",
+
+
+        ]
+    },
+    {
+        id: 12,
+        images: [
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_1.webp",
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_2.webp",
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_3.webp",
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_4.webp",
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_5.webp",
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_6.webp",
+            "./public/images/projects/07_Bellyofthebeast/Nitish-Jain_belly-of-the-beast_7.webp",
+
+
+        ]
+    },
+    {
+        id: 13,
+        images: [
+            "./public/images/projects/13_Midsummer/Nitish-Jain_A-Midsummer-Nights-dream_1.webp",
+            "./public/images/projects/13_Midsummer/Nitish-Jain_A-Midsummer-Nights-dream_2.webp",
+            "./public/images/projects/13_Midsummer/Nitish-Jain_A-Midsummer-Nights-dream_3.webp",
+            "./public/images/projects/13_Midsummer/Nitish-Jain_A-Midsummer-Nights-dream_4.webp",
+            "./public/images/projects/13_Midsummer/Nitish-Jain_A-Midsummer-Nights-dream_5.webp",
+
+        ]
+    },
+    {
+        id: 14,
+        images: [
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_1.webp",
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_2.webp",
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_3.webp",
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_4.webp",
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_5.webp",
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_6.webp",
+            "./public/images/projects/15_Origami cave/Nitish-Jain_India-International-Jewellery-Show_2014©Atelier-Anonyme-Design_7.webp",
+
+
+
+        ]
+    },
+    {
+        id: 15,
+        images: [
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_1-.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_2.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_3.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_4.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_5.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_6.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_7.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_8.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_9.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_10.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_11.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_12.webp",
+            "./public/images/projects/14_tunnels/Nitish-Jain_India-International-Jewellery-Show_©Atelier-Anonyme-Design_13.webp",
+
+
+        ]
+    }
+
+
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    galleriesData.forEach(galleryData => {
+        const section = document.getElementById(`gallery${galleryData.id}`);
+        section.innerHTML = `
+            <div class="indicators" id="indicators${galleryData.id}"></div>
+            <div class="image_section gallery">
+                ${galleryData.images.map((src, index) => `<a href="${src}" data-index="${index}"><img src="${src}" alt=""></a>`).join('')}
+            </div>
+        `;
+
+        lightGallery(section.querySelector('.image_section'), {
+            download: false,
+            fullscreen: false,
+        });
+
+        const imageSection = section.querySelector('.image_section');
+        const indicators = section.querySelector(`#indicators${galleryData.id}`);
+        const images = imageSection.getElementsByTagName('img');
+        const numImages = images.length;
+        let currentIndex = 0;
+
+        function updateIndicators() {
+            indicators.innerHTML = '';
+            for (let j = 0; j < numImages; j++) {
+                const indicator = document.createElement('div');
+                if (j === currentIndex) {
+                    indicator.classList.add('indicate');
+                }
+                indicator.addEventListener('click', () => {
+                    currentIndex = j;
+                    scrollToImage();
+                });
+                indicators.appendChild(indicator);
+            }
+        }
+
+        function scrollToImage() {
+            const currentImage = images[currentIndex];
+            const offsetLeft = currentImage.parentElement.offsetLeft;
+            imageSection.scrollTo({
+                left: offsetLeft,
+                behavior: 'smooth'
+            });
+
+            updateIndicators();
+        }
+
+        function updateCurrentIndex() {
+            const sectionWidth = imageSection.clientWidth;
+            for (let i = 0; i < images.length; i++) {
+                if (images[i].offsetLeft < imageSection.scrollLeft + sectionWidth / 2) {
+                    currentIndex = i;
+                }
+            }
+            updateIndicators();
+        }
+
+        // Scroll event listener
+        imageSection.addEventListener('scroll', () => {
+            updateCurrentIndex();
+        });
+
+        window.addEventListener('resize', scrollToImage);
+
+        updateIndicators();
+        scrollToImage();
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var popupBtns = document.querySelectorAll('.popup-btn');
+    var popupWrap = document.querySelector('.popup-wrap');
+    var popupBox = document.querySelector('.popup-box');
+    var popupClose = document.querySelector('.popup-close');
+
+    popupBtns.forEach(function(btn) {
+        btn.addEventListener('click', function (e) {
+            popupWrap.style.display = 'block';
+            setTimeout(function() {
+                popupBox.classList.remove('transform-out');
+                popupBox.classList.add('transform-in');
+            }, 1); // Slight delay to ensure the display change is applied
+            e.preventDefault();
+        });
+    });
+
+    popupClose.addEventListener('click', function (e) {
+        popupWrap.style.display = 'none';
+        popupBox.classList.remove('transform-in');
+        popupBox.classList.add('transform-out');
+        e.preventDefault();
+    });
+});
+
+
+
+
+// Select all slider wrapper elements
+document.querySelectorAll('.slider-wrapper').forEach(function(sliderWrapper, index) {
+    // Create unique pagination and navigation selectors for each slider
+    const swiper = new Swiper(sliderWrapper, {
+      loop: true,
+      grabCursor: true,
+      spaceBetween: 30,
+      // Pagination bullets
+      pagination: {
+        el: sliderWrapper.querySelector('.swiper-pagination'),
+        clickable: true,
+        dynamicBullets: true
+      },
+      // Navigation arrows
+      navigation: {
+        nextEl: sliderWrapper.querySelector('.swiper-button-next'),
+        prevEl: sliderWrapper.querySelector('.swiper-button-prev'),
+      },
+      // Responsive breakpoints
+      breakpoints: {
+        0: {
+          slidesPerView: 2
+        },
+        768: {
+          slidesPerView: 2
+        },
+       
+        1025: {
+          slidesPerView: 4
+        }
+      }
+    });
+  });
+  
